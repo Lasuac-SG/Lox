8d8ecb3ae3343df0a6bfdd7680cfd80d19a44a81
Lasuac <3240104283@zju.edu.cn>
2026-03-26 15:54:25 +0800

    parser

---
+++ lox/Parser.java
@@ -0,0 +1,16 @@
+package lox;
+
+import java.util.List;
+
+class Parser {
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+}
